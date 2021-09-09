0bba78c91d08b4ecdc48688b29e4aab558ffaae9
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-09 22:05:43 +0200

    Initial Commit

---
+++ Shared/OnBoardingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingAnimationApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 09/09/21.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
